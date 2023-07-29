c59e90fb147c77b7b3ba0c1150b1fef57a695553
brandon <ewduaramadov@gmail.com>
2023-07-29 00:07:22 -0500

    miscelania 2

---
+++ EJERCICIOSMISCELANIAS/MISCELANIA_2/ejercicio15.py
@@ -0,0 +1,8 @@
+#15. Diseñar e implementar un programa que solicite a su usuario un valor no negativo n
+#y visualice la siguiente salida:  
+#1 2 3 ........ n-1 n  
+#1 2 3 ........ n-1  
+#.........  
+#1 2 3  
+#1 2  
+#1
